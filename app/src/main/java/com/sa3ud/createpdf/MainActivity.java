862b84f3ac1ed5b6921ccb0d30f9467ff5b51665
saud <alshabibo@gmail.com>
2021-07-06 14:36:37 +0400

    first

--- app/src/main/java/com/sa3ud/createpdf/MainActivity.java
+++ app/src/main/java/com/sa3ud/createpdf/MainActivity.java
@@ -3,11 +3,14 @@ package com.sa3ud.createpdf;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.view.GravityCompat;
+import androidx.fragment.app.Fragment;
 
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 
@@ -16,7 +19,8 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private static final int TIME_DELAY = 2000;
+    private static long back_pressed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,5 +41,33 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private Fragment getCurrentFragment() {
+
+        return getSupportFragmentManager().findFragmentById(R.id.fl_host);
+
+    }
+
+
+
+    @Override
+    public void onBackPressed() {
+
+      if (getCurrentFragment() instanceof HomeFragment) {
+
+            if (back_pressed + TIME_DELAY > System.currentTimeMillis()) {
+                // exitDialog();
+                super.onBackPressed();
+            } else {
+                Toast.makeText(getBaseContext(), "Press once again to exit!",
+                        Toast.LENGTH_SHORT).show();
+            }
+            back_pressed = System.currentTimeMillis();
+
+        } else {
+
+            super.onBackPressed();
+        }
+
+    }
 
 }
